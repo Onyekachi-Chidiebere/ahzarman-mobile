ffb7598613397f785817aa75dec7df9b39378f98
Onyekachi-Chidiebere <onyekachisamuel18@gmail.com>
2026-07-19 13:28:27 +0200

    added ios otp

--- ios/ahzarman/AppDelegate.swift
+++ ios/ahzarman/AppDelegate.swift
@@ -3,6 +3,7 @@ import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
 import FirebaseCore
+import FirebaseAuth
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,6 +21,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       FirebaseApp.configure()
     }
 
+    // Silent APNs for Firebase Phone Auth (falls back to reCAPTCHA if unavailable)
+    application.registerForRemoteNotifications()
+
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
@@ -36,6 +40,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     )
 
     return true
+  }
+
+  // Handle Firebase Phone Auth reCAPTCHA redirect back into the app
+  func application(
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+  ) -> Bool {
+    if Auth.auth().canHandle(url) {
+      return true
+    }
+    return false
+  }
+
+  func application(
+    _ application: UIApplication,
+    didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+    fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
+  ) {
+    if Auth.auth().canHandleNotification(userInfo) {
+      completionHandler(.noData)
+      return
+    }
+    completionHandler(.noData)
+  }
+
+  func application(
+    _ application: UIApplication,
+    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+  ) {
+#if DEBUG
+    Auth.auth().setAPNSToken(deviceToken, type: .sandbox)
+#else
+    Auth.auth().setAPNSToken(deviceToken, type: .prod)
+#endif
   }
 }
 
